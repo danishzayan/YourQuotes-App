7b2c1d8cc3a8bbe8eb4e4799a66832228ba417fa
Neesh774 <Kanishq0106@gmail.com>
2022-10-02 22:14:48 -0400

    responsive search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,23 @@ import Search from "./Search";
 const Header = ({ handleToggleDarkMode, setSearch }) => {
   return (
     <div className="header">
-      <div className="header-left">
-        <h1>YourQuotes</h1>
-        <Search handleSearchNote={setSearch} />
-      </div>
-      <label className="switch">
-        <input
-          type="checkbox"
-          onClick={() =>
-            handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
-          }
-        />
+      <div className="header-top">
+        <div className="header-left">
+          <h1>YourQuotes</h1>
+          <Search handleSearchNote={setSearch} className="sm-hide" />
+        </div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            onClick={() =>
+              handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+            }
+          />
 
-        <span className="slider round"></span>
-      </label>
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <Search className="sm-show" handleSearchNote={setSearch} />
     </div>
   );
 };
