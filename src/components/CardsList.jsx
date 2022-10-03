1dd5dc3616ec9151124f4937bc5e6430e914153e
Patiphol Pussawong <patiphol.pussawong@ibm.com>
2022-10-03 23:04:00 +0700

    fix: show latest quote first (issue #29)

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -3,9 +3,10 @@ import Card from "./Card";
 import AddNote from "./addNote";
 
 const CardsList = ({ notes, handleAddNote, handleDeleteNote }) => {
+
   return (
     <div className="cards-list">
-      {notes.map((note) => (
+      {[...notes].reverse().map((note) => (
         <Card
           id={note.id}
           color1={note.color1}
